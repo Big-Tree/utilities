1db20bb0656b840820714cd0275af808accf1fef
Steam Deck User <deck@arch.steamdeck>
2023-01-16 19:58:08 +0800

    Handles nvidia-smi not installed

--- GetGpuMemUsage.py
+++ GetGpuMemUsage.py
@@ -4,6 +4,10 @@ import os
 
 def get_gpu_mem_usage():
     command = "nvidia-smi --query-gpu=memory.used --format=csv"
-    memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
+    try:
+        memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
+    except:
+        print('nvidia-smi command failed')
+        return -1
     memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
     return memory_free_values[0]
