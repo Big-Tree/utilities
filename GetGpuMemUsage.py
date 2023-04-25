e30930cdf2432ae8baf984c7a00e0e05ec36e713
William Murphy <william.murphy333@gmail.com>
2023-04-25 17:30:52 +0100

    Works with CSF

--- GetGpuMemUsage.py
+++ GetGpuMemUsage.py
@@ -2,7 +2,7 @@ import subprocess as sp
 import os
 
 
-def get_gpu_mem_usage(gpu_num=0):
+def get_gpu_mem_usage():
     command = "nvidia-smi --query-gpu=memory.used --format=csv"
     try:
         memory_free_info = sp.check_output(command.split()).decode('ascii').split('\n')[:-1][1:]
@@ -10,6 +10,9 @@ def get_gpu_mem_usage(gpu_num=0):
         print('nvidia-smi command failed')
         return -1
     memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
-    if gpu_num == '?':
+
+    if 'CUDA_VISIBLE_DEVICES' in os.environ:
         gpu_num = int(os.environ['CUDA_VISIBLE_DEVICES'])
+    else:
+        gpu_num = 0
     return memory_free_values[gpu_num]
